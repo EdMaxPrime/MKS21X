fe32ee4d6518ff2c0c94c04c0fb8942f3a4fb8cd
Max Z <max.zlotskiy@gmail.com>
2016-12-05 19:39:40 -0500

    Added main and toString

--- SlidingPuzzle/SlidingPuzzle.java
+++ SlidingPuzzle/SlidingPuzzle.java
@@ -21,4 +21,21 @@ public class SlidingPuzzle {
 	    }
 	}
     }
+
+    public String toString() {
+	String str = "";
+	for(int row = 0; row < side; row++) {
+	    for(int col = 0; col < side; col++) {
+		str += (values[row][col] + " ");
+	    }
+	    str += "\n";
+	}
+	return str;
+    }
+
+    public static void main(String[] args) {
+	SlidingPuzzle s = new SlidingPuzzle(4);
+	System.out.println(s);
+	//System.out.println();
+    }
 }

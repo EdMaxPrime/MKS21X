8b264fb60cb17487cc1b7ad1c61aef3727c40312
Max Z <max.zlotskiy@gmail.com>
2016-12-05 19:36:40 -0500

    Added initialize method

--- SlidingPuzzle/SlidingPuzzle.java
+++ SlidingPuzzle/SlidingPuzzle.java
@@ -1,4 +1,24 @@
+import java.util.*;
+
 public class SlidingPuzzle {
-    private int sides;
+    private int side;
     private int[][] values;
+
+    public SlidingPuzzle(int sidelength) {
+	side = sidelength;
+	values = new int[side][side];
+    }
+
+    public void initialize() {
+	ArrayList<Integer> numbers = new ArrayList<Integer>();
+	for(int n = 0; n < side * side; n++)
+	    numbers.add(new Integer(n));
+	for(int row = 0; row < side; row++) {
+	    for(int col = 0; col < side; col++) {
+		int index = (new Random()).nextInt(numbers.size());
+		values[row][col] = numbers.get(index);
+		numbers.remove(index);
+	    }
+	}
+    }
 }

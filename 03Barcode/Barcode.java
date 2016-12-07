69e8e05fdbf70e0becb3c6450971da015428fe84
Max Z <max.zlotskiy@gmail.com>
2016-12-07 03:18:39 -0500

    Wrote constructor, throws exceptions

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -2,7 +2,18 @@ public class Barcode {
     private String _zip;
     private int _checkDigit;
 
-    public Barcode(String zip) {}
+    public Barcode(String zip) {
+	if(zip.length() != 5) {
+	    throw new IllegalArgumentException("ZIP code must have a length of 5");
+	}
+	for(int i = 0; i < 5; i++) {
+	    if(!Character.isDigit(zip.charAt(i))) {
+		throw new IllegalArgumentException("ZIP code must be a 5 digit whole number");
+	    }
+	}
+	_zip = zip;
+	_checkDigit = checkSum();
+    }
 
     public Barcode clone() {return null;}
 

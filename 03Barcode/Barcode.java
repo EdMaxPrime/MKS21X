6dbce9c210c0d5ec7af787170185adea0e0e2969
Max Z <max.zlotskiy@gmail.com>
2016-12-07 03:10:02 -0500

    Wrote skeleton (compiles)

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,3 +1,14 @@
 public class Barcode {
-    
+    private String _zip;
+    private int _checkDigit;
+
+    public Barcode(String zip) {}
+
+    public Barcode clone() {return null;}
+
+    private int checkSum() {return 0;}
+
+    public int compareTo(Barcode other) {return 0;}
+
+    public String toString() {return "";}
 }
